53d18f8758afb0ed47bd476332780c5f2fcc1114
Simon Marti <simon.marti@inventage.com>
2026-03-18 06:22:08 +0000

    fix: [F-063] suppress docker info output in DockerAvailable

--- internal/docker/docker.go
+++ internal/docker/docker.go
@@ -11,6 +11,8 @@ import (
 
 func DockerAvailable() error {
 	cmd := exec.Command("docker", "info")
+	cmd.Stdout = nil
+	cmd.Stderr = nil
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("docker daemon not running (is Docker installed and started?): %w", err)
 	}
